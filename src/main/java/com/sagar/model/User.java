0a9b612ca79fe1c6302894d0c8e2669c589c8f34
2000sagarr <2000sagarr@gmail.com>
2024-06-18 10:48:34 +0530

    Added all

---
+++ src/main/java/com/sagar/model/User.java
@@ -0,0 +1,50 @@
+package com.sagar.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    private String role;
+    private String mobile;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Address> address = new ArrayList<>();
+
+    @Embedded
+    @ElementCollection
+    @CollectionTable(name = "payment_information", joinColumns = @JoinColumn(name="user_id"))
+    private List<PaymentInformantion> paymentInformantions = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Rating> rating =  new ArrayList<>();
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Review> reviews =  new ArrayList<>();
+
+    private LocalDateTime createdAt;
+
+}
---
+++ src/test/java/com/sagar/ecommercespring/ECommerceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagar.ecommercespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
